e7a32ecacda66ed41d2ab9849d7dfbfeec34c3af
zub1984 <zub1984.kn@gmail.com>
2016-01-03 04:13:07 +0530

    [Football] : updated copyright license

--- Football/app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ Football/app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -21,9 +21,20 @@ import barqsoft.footballscores.utils.Constants;
 import barqsoft.footballscores.utils.FootballUtils;
 import barqsoft.footballscores.utils.Utility;
 
-/**
- * Created by yehya khaled on 2/27/2015.
- */
+/*Copyright (C) 2015  Mohammad Jubair Khan (zub1984.kn@gmail.com) - Football Scores Project of Udacity Nanodegree course.
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.*/
+
 public class PagerFragment extends Fragment {
     public static final int NUM_PAGES = 5;
     public ViewPager mPagerHandler;
--- Football/app/src/main/java/barqsoft/footballscores/utils/Constants.java
+++ Football/app/src/main/java/barqsoft/footballscores/utils/Constants.java
@@ -2,9 +2,19 @@ package barqsoft.footballscores.utils;
 
 import barqsoft.footballscores.data.DatabaseContract;
 
-/**
- * Created by laptop on 12/28/2015.
- */
+/*Copyright (C) 2015  Mohammad Jubair Khan (zub1984.kn@gmail.com) - Football Scores Project of Udacity Nanodegree course.
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.*/
 public class Constants {
     public static final int CONNECTION_AND_READ_TIME_OUT = 5;
 
